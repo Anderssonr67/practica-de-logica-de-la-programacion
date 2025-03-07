5beeaad07f5a6413c1eb320f5a68f0327702ecd3
Andersson Wilfredo <vargasandersson04@gmail.com>
2025-03-07 17:29:48 -0600

    practicas

---
+++ practica de laboratorio logica de  programacion/practica3/app.js
@@ -0,0 +1,23 @@
+const sumar = require("./sumar");
+const readline = require("readline")
+
+const InterfazConsola = readline.CreaInterface({
+    input : processingInstruction.stdin,
+    output : process.stdout
+})
+
+InterfazConsola.question("porfavor ingrese el primer numero", (num1) => {
+
+    InterfazConsola.question("porfavor ingrese el segundo numero", (num2) => {
+
+        let numero1 = parseFloat(num1)
+        let numero2 = parseFloat(num2)
+
+        console.log(`Su suma es : ${sumar(numero1, numero2)}`);
+        
+    
+
+    })
+})
+
+
---
+++ practica de laboratorio logica de  programacion/practica3/app.ts
@@ -0,0 +1,15 @@
+
+
+let names : string = "Andersson"
+
+let age : number = 18
+
+let activo: boolean = true
+
+interface User {
+    name : string,
+    age : number,
+    email : string,
+}
+
+const listUser: User [] = []
